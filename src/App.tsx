76f03bdbe015ea011b70fcda6e6cc9295517b158
ddurant <ddurant@thoughtwaves.com>
2022-06-02 11:22:45 -0400

    change uploadEndpoint to use an env variable

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,12 @@ import About from "./pages/About"
 import { Route, Routes } from "react-router-dom"
 
 function App() {
-  const uploadEndpoint = 'http://localhost:8080/salesVis/importData';
+  const uploadEndpoint = process.env.REACT_APP_UPLOAD_ENDPOINT;
 
   return (
     <>
       <Navbar />
+      <p>Application running in <b>{process.env.NODE_ENV}</b> mode.</p>
       <div className="container">
         <Routes>
           <Route path="/" element={<Home />} />
